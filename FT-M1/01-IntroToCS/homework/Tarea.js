07a677cc704275fa857c12f4b190f82729db298f
Emmanuel Mercado <Emmanuel.Mercado@flex.com>
2023-04-12 12:47:48 -0600

    Tarea 1 listo

--- FT-M1/01-IntroToCS/homework/Tarea.js
+++ FT-M1/01-IntroToCS/homework/Tarea.js
@@ -13,3 +13,29 @@ function BinarioADecimal(num) { // Recibe un String y retorna num
     return resultado;
 }
 BinarioADecimal('01111')
+
+
+function DecimalABinario(num) {
+    //Convertir de Decimal a Binario
+    //Dividir entre 2 hasta sea 0 y tomar los residuos
+    //36 / 2 = 18 Residuo 0
+    //18 / 2 = 9 Residuo 0
+    //9 / 2 = 4 Residuo 1
+    //4 / 2 = 2 Residuo 0
+    //2 / 2 = 1 Residuo 0
+    //1 / 2 = 0 Residuo 1
+    //100100
+
+    let resultado = "";
+    while(num>0){
+        if(num%2==0){
+            resultado = '0'+resultado
+        }
+        else{
+            resultado = '1'+resultado
+        }
+        console.log(resultado);
+        num=Math.floor(num/2) 
+    }
+}
+DecimalABinario(36);
