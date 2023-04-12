f11b3fb1204673ffb34bef36622adfba19b14a83
Emmanuel Mercado <Emmanuel.Mercado@flex.com>
2023-04-12 08:37:17 -0600

    Ejercicio 1 Tarea 1 Listo

--- FT-M1/01-IntroToCS/homework/Tarea.js
+++ FT-M1/01-IntroToCS/homework/Tarea.js
@@ -1,7 +1,5 @@
-'use strict';
-
 function BinarioADecimal(num) { // Recibe un String y retorna num
- //Convertir de Binario a decimal
+    //Convertir de Binario a decimal
     //Recibo un 0111 y regreso un 7
     // 2 elevado a la posición por el número
     // 2**posición menos significativa * Número
@@ -10,14 +8,8 @@ function BinarioADecimal(num) { // Recibe un String y retorna num
 
     for (let i = num.length-1; i>=0; i--){
         resultado += (2**i)*num[num.length-1-i]
-        //console.log(resultado);
+        console.log(resultado);
     }
     return resultado;
- }
-function DecimalABinario(num) {}
-
-module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
-};
-// 8 en binario = 1000
+}
+BinarioADecimal('01111')
